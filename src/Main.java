4baa4aa151ad28846af93d11f23dc46f319c6468
Neto testt <testt@neto.com>
2023-03-07 16:21:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 13676;
+        int bonusMile = 20;
+        int bonus = priceTicket / bonusMile;
+
+        System.out.println("При стоимости билета в "+priceTicket+" рублей должно начислиться "+bonus+" бонус(ов).");
+
+    }
+}
